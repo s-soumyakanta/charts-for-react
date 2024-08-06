aebc53a76b43d28d3d45f334b5c78a74eca0959d
S Soumyakanta <ssoumyakanta@fightright.in>
2024-08-06 07:56:45 +0530

    initial commit with routing with layout.

---
+++ src/app/apexchart/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ApexChartPage = () => {
+  return (
+    <div>ApexChartPage</div>
+  )
+}
+
+export default ApexChartPage
---
+++ src/components/Navabar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navabar = () => {
+  return (
+    <nav className='flex w-full bg-gray-900 p-4 justify-between'>
+       <div>
+        <Link href="/">
+       <h1 className='text-lg'>Charts React <span className='text-sm'>- by <Link className='underline' target='_blank' href='https://s-soumyakanta.com'>S Soumyakanta</Link></span></h1>
+        </Link>
+       </div>
+
+       <div>
+        <Link href="/">
+        <button>Back</button>
+        </Link>
+       </div>
+    </nav>
+  )
+}
+
+export default Navabar
