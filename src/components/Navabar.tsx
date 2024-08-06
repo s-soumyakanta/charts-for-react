67747885285f4f8f8efb312314be6c2feab25ebe
S Soumyakanta <ssoumyakanta@fightright.in>
2024-08-06 08:11:31 +0530

    Portfolio links fixed

--- src/components/Navabar.tsx
+++ src/components/Navabar.tsx
@@ -4,10 +4,11 @@ import React from 'react'
 const Navabar = () => {
   return (
     <nav className='flex w-full bg-gray-900 p-4 justify-between'>
-       <div>
+       <div className='flex items-center space-x-1'>
         <Link href="/">
-       <h1 className='text-lg'>Charts For React <span className='text-sm'>- by <Link className='underline' target='_blank' href='https://s-soumyakanta.com'>S Soumyakanta</Link></span></h1>
+       <h1 className='text-lg'>Charts For React </h1>
         </Link>
+        <p className='text-sm'>- by <Link className='underline' target='_blank' href='https://s-soumyakanta.com'>S Soumyakanta</Link></p>
        </div>
 
        <div>
